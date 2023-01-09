ee27bc6239540bd5c487c21f202663a3e482ddcb
fernando26f <74388127+fernando26f@users.noreply.github.com>
2023-01-09 07:43:28 +0000

    Ejercicio1.6

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  // Operaciones
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1> give feedback </h1>
+      <div >
+        <button onClick={() => setGood(good + 1)}>
+          Good
+        </button>
+        <button onClick={() => setNeutral(neutral + 1)}>
+          Neutral
+        </button>
+        <button onClick={() => setBad(bad + 1)}>
+          Bad
+        </button>
+      </div>
+      <div>
+        <h1>statics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average}</p>
+        <p>positive {positive} %</p>
+      </div>
     </div>
   );
 }
